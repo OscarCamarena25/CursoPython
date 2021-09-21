422fee1abc07dba4de3be1a4c23710fdaa2afc84
Oscar <oscarcam879o@gmail.com>
2021-09-21 17:23:07 -0500

    Mi primer programa

---
+++ hola_mundo.py
@@ -0,0 +1,24 @@
+
+message1 = "hola mundo"
+message2 = "Adios Mundo"
+print(message1)
+print(message2)
+print(message1.upper())
+
+
+
+
+nombre = "oscar"
+apellido = "camarena"
+
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
+
+
+nombre_completo_f_string = f"{nombre} {apellido}"
+print(nombre_completo_f_string)
+
+
+
+texto = "uno\ndos\ntres"
+print(texto)
